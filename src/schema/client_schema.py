49b9a52c182534365228e492666a2446939f7917
logg1n <loggiin13@gmail.com>
2026-03-13 10:30:28 +0300

    new: pydantic schema for openai

---
+++ src/schema/client_schema.py
@@ -0,0 +1,29 @@
+from typing import Literal
+
+from pydantic import (
+    BaseModel,
+    ConfigDict,
+    Field,
+    HttpUrl,
+    SecretStr,
+)
+
+
+class PropertiesModelaAi(BaseModel):
+    temperature: float
+    max_token: int = Field(min_length=1, max_length=1000)
+    # request_token: int
+    # response_token: int
+    tool_choise: Literal["none", "auto", "required"] = Field(default="auto")
+
+    model_config = ConfigDict(extra="ignore")
+
+
+class BaseClient(BaseModel):
+    base_url: HttpUrl
+    model: str
+    api_key: SecretStr
+    ai_model: str
+    role: str
+
+    model_config = ConfigDict(extra="ignore")
